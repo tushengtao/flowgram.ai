f10120e015ac23c2b11f838cd0b4293e207b24b1
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2025-06-11 16:42:01 +0800

    feat: 使用后端服务器运行

--- apps/demo-free-layout/src/hooks/use-editor-props.tsx
+++ apps/demo-free-layout/src/hooks/use-editor-props.tsx
@@ -277,13 +277,13 @@ export function useEditorProps(
          */
         createContextMenuPlugin({}),
         createRuntimePlugin({
-          mode: 'browser',
-          // mode: 'server',
-          // serverConfig: {
-          //   domain: 'localhost',
-          //   port: 4000,
-          //   protocol: 'http',
-          // },
+          // mode: 'browser',
+          mode: 'server',
+          serverConfig: {
+            domain: 'localhost',
+            port: 4000,
+            protocol: 'http',
+          },
         }),
       ],
     }),
